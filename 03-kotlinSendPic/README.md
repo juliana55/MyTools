0dc7ec072f2aa69bc498817b8a9e6dca02489950
Juliana55 <blesszj55@hotmail.com>
2019-06-06 19:56:12 +0900

    kotlinでmaasに画像ファイルアップ

---
+++ 03-kotlinSendPic/README.md
@@ -0,0 +1,160 @@
+# 【Android】写真をクラウドに保存しよう！ for Kotlin
+
+![画像1](/readme-img/OverView.png)
+
+## 概要
+
+* [ニフクラ mobile backend](https://mbaas.nifcloud.com/) の『ファイルストア機能』を利用して、「撮った写真をクラウドに保存する」内容を実装したサンプルプロジェクトです
+* 簡単な操作ですぐに [ニフクラ mobile backend](https://mbaas.nifcloud.com/)の機能を体験いただけます
+
+## ニフクラ mobile backendって何？？
+スマートフォンアプリのバックエンド機能（プッシュ通知・データストア・会員管理・ファイルストア・SNS連携・位置情報検索・スクリプト）が**開発不要**、しかも基本**無料**(注1)で使えるクラウドサービス！今回はデータストアを体験します
+
+注1：詳しくは[こちら](https://mbaas.nifcloud.com/price.htm)をご覧ください
+
+![画像2](/readme-img/SdkTypes.png)
+
+## 動作環境
+* Windows 7 Professional
+* Android Studio 3.1
+* Android ver 4x,5x
+  - このサンプルアプリは、端末のカメラを使用するため、実機が必要です
+* Android SDK v3
+  - SDK v2系だと動作しないので注意
+
+※上記内容で動作確認をしています
+
+
+## 手順
+### 1. ニフクラ mobile backend の会員登録・ログインとアプリの新規作成
+* 下記リンクから会員登録（無料）をします
+  * https://console.mbaas.nifcloud.com/signup
+* 登録ができたら下記リンクからログインします
+  * https://console.mbaas.nifcloud.com/
+* 下図のように「アプリの新規作成」画面が出るのでアプリを作成します
+  * 既に mobile backend を利用したことがある方は左上の「＋新しいアプリ」をクリックすると同じ画面が表示されます
+
+![画像3](/readme-img/mBassNewProject.png)
+
+* アプリ作成されると下図のような画面になります
+* この２種類のAPIキー（アプリケーションキーとクライアントキー）は先ほどインポートしたAndroidStudioで作成するAndroidアプリにニフクラ mobile backendの紐付けるため、あとで使います
+
+![画像4](/readme-img/mBassAPIkey.png)
+
+* 動作確認後に写真（画像）が保存される場所も確認しておきましょう
+
+![画像5](/readme-img/mBassData.png)
+
+### 2. サンプルプロジェクトのダウンロード
+* 下記リンクをクリックしてプロジェクトをダウンロードします
+  * https://github.com/NiFCloud-mbaas/KotlinFileApp/archive/master.zip
+* ダウンロードしたプロジェクトを解凍します
+* AndroidStudio を開きます、「Open an existing Android Studio projct」をクリックして解凍したプロジェクトを選択します
+
+![画像6](/readme-img/SelectProject.png)
+
+* プロジェクトを開きます
+
+![画像7](/readme-img/ProjectDesign.png)
+
+### 3. SDKの導入（実装済み）
+
+※このサンプルアプリには既にSDKが実装済み（下記手順）となっています。（ver.3.0.0)<br>　最新版をご利用の場合は入れ替えてご利用ください。
+
+* SDKダウンロード
+SDKはここ（[SDK リリースページ](https://github.com/NIFCloud-mbaas/ncmb_android/releases)）から取得してください.
+  - NCMB.jarファイルがダウンロードします。
+* SDKをインポート
+  - app/libsフォルダにNCMB.jarをコピーします
+* 設定追加
+  - app/build.gradleファイルに以下を追加します
+```gradle
+dependencies {
+    compile 'com.google.code.gson:gson:2.3.1'
+    compile files('libs/NCMB.jar')
+}
+```
+  - androidManifestの設定
+    - <application>タグの直前に以下のpermissionを追加します
+```html
+<uses-permission android:name="android.permission.INTERNET" />
+<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+```
+
+### 4. APIキーの設定
+
+* AndroidStudio で MainActivity.kt を開きます
+  * ディレクトリはデフォルトで「Android」が選択されていますので、「Project」に切り替えてから探してください
+
+![画像09](/readme-img/009.png)
+
+* APIキー（アプリケーションキーとクライアントキー）の設定をします
+
+![画像8](/readme-img/AndroidAPIkey.png)
+
+* それぞれ`YOUR_APP_KEY`と`YOUR_CLIENT_KEY`の部分を書き換えます
+  * このとき、ダブルクォーテーション（`"`）を消さないように注意してください！
+
+### 5. 動作確認
+
+* エミュレーターでアプリをビルドします
+  * 失敗する場合は一度「Clean Project」を実行してから再度ビルドしてください
+* アプリが起動したら
+  1. 「CAMERA」ボタンをタップして、写真を撮影します
+  2. 「保存」ボタンをタップして、保存します
+    - 今回はファイル名固定しています：`test.png`
+* ニフクラ mobile backend 上に画像が保存されると、アプリの画面に画像表示されます
+
+![画像9](/readme-img/AndroidCamera.png)
+
+* 保存に成功したら、[ニフクラ mobile backend](https://mbaas.nifcloud.com/) の管理画面から「ファイルストア」を確認してみましょう！
+
+![画像10](/readme-img/mBassFileStore.png)
+
+* 簡単に写真がクラウドに保存できました
+
+## 解説
+サンプルプロジェクトに実装済みの内容のご紹介します。
+
+* `activity_main.xml`でデザインを作成し、`MainActivity.kt`にロジックを書いています
+
+### ファイルのアップロード
+
+* ファイルストアを利用するには、SDKが提供する `NCMBFile` を使用します
+* ファイルのアップロードを行うには、このクラスが提供する `saveInBackground` メソッドを利用します（非同期処理）
+* ファイル名を固定しているため、新しくファイル（画像）を保存すると上書きされます
+
+```Kotlin
+val file = NCMBFile("test.png", dataByte, acl)
+file.saveInBackground { e ->
+val result: String
+if (e != null) {
+    //保存失敗
+
+} else {
+    //保存成功
+
+}
+```
+
+### アップロードしたファイルを取得
+
+* ファイルストアに保存したものを取得するには、`fetch` メソッドを利用します（同期処理）
+  - `fetchInBackground` メソッドでの非同期処理も利用可能です
+ - ファイルの取得時には、ファイル名を先に取得する必要があります(今回はファイル名固定：`test.png`)
+
+```kotlin
+val file = NCMBFile("test.png")
+file.fetchInBackground { dataFetch, er ->
+   if (er != null) {
+       //失敗処理
+
+   } else {
+       //成功処理
+
+   }
+}
+```
+
+## 参考
+* ニフクラ mobile backend の[ドキュメント（ファイルストア（Android））](https://mbaas.nifcloud.com/doc/current/filestore/basic_usage_android.html)（for Java）をご用意していますので、ご活用ください
---
+++ 03-kotlinSendPic/app/src/main/java/mbaas/com/nifcloud/kotlincameraquickstart/MainActivity.kt
@@ -0,0 +1,170 @@
+package mbaas.com.nifcloud.kotlincameraquickstart
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import com.nifcloud.mbaas.core.NCMB
+import com.nifcloud.mbaas.core.NCMBAcl
+import com.nifcloud.mbaas.core.NCMBFile
+import java.io.ByteArrayOutputStream
+
+
+class MainActivity : AppCompatActivity() {
+    internal var b1: Button? = null
+    internal var b2: Button? = null
+    internal var iv: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //**************** APIキーの設定とSDKの初期化 **********************
+        NCMB.initialize(this, "c229cea695ec78ddfe6a5eb5cd69f11318db14f05d7dc990ac7b8d3408497579", "a51818098d824662b6723bdf86dbce3963dc47abf64796c234dad321dcdacf48");
+
+        setContentView(R.layout.activity_main)
+
+        b1 = findViewById<View>(R.id.button) as Button
+        iv = findViewById<View>(R.id.imageView) as ImageView
+
+        b1?.setOnClickListener { requestCameraPermission() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null || data.extras == null) {
+            return
+        } else {
+            val bp = data.extras!!.get("data") as Bitmap
+            //******* NCMB file upload *******
+            val byteArrayStream = ByteArrayOutputStream()
+            bp.compress(Bitmap.CompressFormat.PNG, 0, byteArrayStream)
+            val dataByte = byteArrayStream.toByteArray()
+
+            //ACL 読み込み:可 , 書き込み:可
+            val acl = NCMBAcl()
+            acl.publicReadAccess = true
+            acl.publicWriteAccess = true
+
+            //通信実施
+            val file = NCMBFile("test.png", dataByte, acl)
+            file.saveInBackground { e ->
+                val result: String
+                if (e != null) {
+                    //保存失敗
+                    AlertDialog.Builder(this@MainActivity)
+                            .setTitle("Notification from NIFCloud")
+                            .setMessage("Error:" + e.message)
+                            .setPositiveButton("OK", null)
+                            .show()
+                } else {
+                    //******* NCMB file download *******
+                    val file = NCMBFile("test.png")
+                    file.fetchInBackground { dataFetch, er ->
+                        if (er != null) {
+                            //失敗処理
+                            AlertDialog.Builder(this@MainActivity)
+                                    .setTitle("Notification from NIFCloud")
+                                    .setMessage("Error:" + er.message)
+                                    .setPositiveButton("OK", null)
+                                    .show()
+                        } else {
+                            //成功処理
+                            val bMap = BitmapFactory.decodeByteArray(dataFetch, 0, dataFetch.size)
+                            iv?.setImageBitmap(bMap)
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    private fun requestCameraPermission() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                        // permission is granted
+                        openCamera()
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                        // check for permanent denial of permission
+                        if (response.isPermanentlyDenied) {
+                            showSettingsDialog()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest, token: PermissionToken) {
+                        token.continuePermissionRequest()
+                    }
+                }).check()
+    }
+
+    private fun showSettingsDialog() {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Need Permissions")
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
+            dialog.cancel()
+            openSettings()
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
+
+    }
+
+    // navigating user to app settings
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivityForResult(intent, 101)
+    }
+
+    private fun openCamera() {
+        val intent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+}
